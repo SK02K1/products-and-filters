6a3bc3f64e0c1071aef034cabe9d28140e39fe1d
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-03-11 21:21:32 +0530

    fix: replaced class keyword with className

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,19 +1,19 @@
 export const ProductCard = ({ itemInfo: { name, price, img, rating } }) => {
   return (
-    <div class="card">
-      <span class="material-icons-outlined card-icon-like">
+    <div className="card">
+      <span className="material-icons-outlined card-icon-like">
         favorite_border
       </span>
-      <div class="card-header m-xs-tb">
-        <img class="card-img m-xs-tb" src={img} alt={name} />
+      <div className="card-header m-xs-tb">
+        <img className="card-img m-xs-tb" src={img} alt={name} />
       </div>
-      <div class="card-body m-xs-tb">
-        <h2 class="item-price m-xs-tb">${price} USD</h2>
-        <h3 class="card-title m-xs-tb">{name}</h3>
+      <div className="card-body m-xs-tb">
+        <h2 className="item-price m-xs-tb">${price} USD</h2>
+        <h3 className="card-title m-xs-tb">{name}</h3>
         <h4 className="text-sm">{rating}/5</h4>
       </div>
-      <div class="card-footer m-xs-tb">
-        <button class="btn btn-secondary card-btn" href="#">
+      <div className="card-footer m-xs-tb">
+        <button className="btn btn-secondary card-btn" href="#">
           Add to cart
         </button>
       </div>
